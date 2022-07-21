3200598082d27608cf27e8c4326cfce6ce8f8255
guilherme <guilherme.pereira1680@gmail.com>
2022-07-21 16:45:23 -0300

    git commit

--- core/serializers.py
+++ core/serializers.py
@@ -1,10 +1,22 @@
-from dataclasses import fields
 from rest_framework.serializers import ModelSerializer
 
-from core.models import Categoria
+from core.models import Categoria, Editora, Livro
 
 
 class CategoriaSerializer(ModelSerializer):
     class Meta:
         model = Categoria
         fields = "__all__"
+
+
+class EditoraSerializer(ModelSerializer):
+    class Meta:
+        model = Editora
+        fields = "__all__"
+
+
+class LivroSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+        depth = 1
