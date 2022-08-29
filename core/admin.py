3a4a249a875b14d2c795e003499adb1d7c335fda
= <=>
2022-08-29 14:07:00 -0300

    commit

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db import models
 
 from core.models import Autor, Categoria, Editora, Livro
 
@@ -6,3 +7,17 @@ admin.site.register(Autor)
 admin.site.register(Categoria)
 admin.site.register(Editora)
 admin.site.register(Livro)
+
+
+class Marca(models.Model):
+    nome = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+
+
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
