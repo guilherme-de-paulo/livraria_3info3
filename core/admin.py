233ecd96e123f934c694b6bc24008ff82c9f583a
Guilherme de Paulo Pereira <guilherme.pereira1680@gmail.com>
2022-09-19 14:48:02 -0300

    commit

--- core/admin.py
+++ core/admin.py
@@ -7,17 +7,3 @@ admin.site.register(Autor)
 admin.site.register(Categoria)
 admin.site.register(Editora)
 admin.site.register(Livro)
-
-
-class Marca(models.Model):
-    nome = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.nome
-
-
-class Categoria(models.Model):
-    descricao = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.descricao
